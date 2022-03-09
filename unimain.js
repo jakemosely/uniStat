865fbb2a5c2a5f90d3129bb3b3ce53b520d66be4
Jake Mosely <jakemosely@gmail.com>
2022-03-09 14:44:23 -0500

    Add files via upload

--- unimain.js
+++ unimain.js
@@ -1,7 +1,37 @@
 $(document).ready(function(){
-  $("#currency").hide();
+  $(".currency").hide();
   $("#sing").click(function(){
     $("#sing").hide();
-    $("#currency").show();
+    $(".currency").show();
   });
 });
+
+var lightSpeed = 3;
+var hydrogen = 750;
+var helium = 247;
+var lithium = 2;
+var beryllium = 1;
+var energy = 0;
+var massONEstar = 0;
+
+
+function displayCounts() {
+document.getElementById('hydrogen').innerHTML = hydrogen;
+document.getElementById('helium').innerHTML = helium;
+document.getElementById('lithium').innerHTML = lithium;
+document.getElementById('beryllium').innerHTML = beryllium;
+document.getElementById('energy').innerHTML = energy;
+};
+
+function massONEstarcreate() {
+  var starCost = 100;
+  if (hydrogen >= starCost) {
+    massONEstar = massONEstar + 1;
+  }
+
+}
+
+window.setInterval(function(){
+  displayCounts();
+
+}, 1000);
